2e4aba3f3892d31c6779f4a36d253918b116efae
ltsipunov <l.tsipunov@yandes.ru>
2025-02-25 22:12:22 +0300

    fixed transparent background, image size target jpegs added

--- main.py
+++ main.py
@@ -11,13 +11,14 @@ icon = pygame.image.load("img/icon.png")
 pygame.display.set_icon(icon)
 
 target_img = pygame.image.load("img/target.png")
-target_width = 80
-target_height = 80
+target_img.set_colorkey((255,255,255))
+# target_img = target_img.convert_alpha()
+target_width = target_img.get_width()
+target_height = target_img.get_height()
 target_x = random.randint(0, SCREEN_WIDTH - target_width)
 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
 color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-
 
 running = True
 while running:
@@ -27,11 +28,13 @@ while running:
             running = False
     if event.type == pygame.MOUSEBUTTONDOWN:
         mouse_x, mouse_y = pygame.mouse.get_pos()
-        if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+        center_x,center_y = target_x + target_width/2, target_y + target_height/2
+        dist = ((center_x-mouse_x)**2 + (center_y-mouse_y)**2)**0.5
+        if dist < (target_width/2)*0.8:
             target_x = random.randint(0, SCREEN_WIDTH - target_width)
             target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
     screen.blit(target_img, (target_x, target_y))
-    pygame.display.update()
+    pygame.display.flip()
 
 pygame.quit()
